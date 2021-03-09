f25f4646d904d4387f64263547c14b9824e8767b
Boyuan Liu <boyuanliu6@yahoo.com>
2021-03-09 09:37:11 -0800

    added role detection

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,14 @@ app.config["SECRET_KEY"] = "secretkey"
 DISCORD_CLIENT_ID = os.getenv("DISCORD_CLIENT_ID")
 DISCORD_CLIENT_SECRET = os.getenv("DISCORD_CLIENT_SECRET")
 REDIRECT_URI = "http://127.0.0.1:5000/discord/callback"
-FELLOWSHIP_GUILD_ID = "716052909271285803"
+FELLOWSHIP_GUILD_ID = "818888976458973224"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CURRENT_FELLOWSHIP = "0"
+
+@app.route("/")
+def index():
+    return f"Hello {session.get('username')}#{session.get('discriminator')} @ {session.get('role')}"
+
 
 @app.route('/discord')
 def discord():
@@ -45,6 +52,11 @@ def discord_callback():
     discord_id = data.json()["id"]
     username = data.json()["username"]
 
+    session["discord_id"] = discord_id
+    session["username"] = username
+    session["email"] = email
+    session["discriminator"] = discriminator
+
     # get all the guilds that user's in
     guilds = requests.get("https://discord.com/api/v8/users/@me/guilds", headers={
         "Authorization": f"Bearer {session.get('discord_access_token')}"
@@ -57,6 +69,30 @@ def discord_callback():
             in_fellowship = True
         elif in_fellowship == False:
             in_fellowship = False
+
+    if not in_fellowship:
+        return "Error, this is for current MLH Fellow's only!"
+
+    if in_fellowship:
+        role = requests.get(f"https://discord.com/api/v8/guilds/{FELLOWSHIP_GUILD_ID}/members/{session.get('discord_id')}", headers={
+            "Authorization": f"Bot {BOT_TOKEN}"
+        })
+
+        # assume user only have one role
+        user_roles = role.json()["roles"]
+
+        roles = requests.get(f"https://discord.com/api/v8/guilds/{FELLOWSHIP_GUILD_ID}/roles", headers={
+            "Authorization": f"Bot {BOT_TOKEN}"
+        })
+
+        role = None
+        for r in roles.json():
+            if r["name"] == "admin" and r["id"] in user_roles:
+                role = "admin"
+            elif r["name"][4] == CURRENT_FELLOWSHIP and r["id"] in user_roles:
+                role = r["name"]
+
+        session["role"] = role
 
     # redirect to homepage
     return redirect("/")
