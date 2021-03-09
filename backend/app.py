ea839b69b14b831d27732536027193df5ace626e
Chau Vu <chv004@ucsd.edu>
2021-03-09 13:09:16 -0800

    Initialized PostgreSQL database

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,26 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+db_uri = 'postgresql://{dbuser}:{dbpw}@{dbhost}:{dbport}/{dbname}'.format(
+    dbuser=os.environ['DB_USER'],
+    dbpw=os.environ['DB_PW'],
+    dbhost=os.environ['DB_HOST'],
+    dbport=os.environ['DB_PORT'],
+    dbname=os.environ['DB_NAME']
+)
+
+app.config.update(
+    SQLALCHEMY_DATABASE_URI=db_uri,
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+)
+
+db = SQLAlchemy(app)
 
 if __name__ == '__main__':
     app.run()
