e4470243382f8c7f5849407c061668d832b8550c
Pawan Kolhe <contact@pawankolhe.com>
2021-03-10 15:07:56 +0530

    feat: animated login screen

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -14,9 +14,9 @@ const LoginPage = (props) => {
       user: {
         discordId: 'PawanKolhe#7887',
         email: 'contact@pawankolhe.com',
-        // role: 'fellow',
-        // pod: '2.0.0',
-        // name: 'Pawan Kolhe'
+        role: 'fellow',
+        pod: '2.0.0',
+        name: 'Pawan Kolhe'
       }
     });
   }
@@ -29,6 +29,8 @@ const LoginPage = (props) => {
           <LoginButton colorCode="#6C89E0" text="Login with Discord" icon={<InlineIcon icon={bxlDiscord} />} onClick={handleLogin} />
         </div>
       </div>
+
+      <div className="stripe"></div>
     </div>
   )
 }
