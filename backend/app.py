357bdb8dfe8a08ea057a13108946a2b24ec0ac72
luiszugasti <luis.zugasti@outlook.com>
2021-03-12 07:19:48 -0600

    Serialize json format, remove unneccessary gitignore

--- backend/app.py
+++ backend/app.py
@@ -4,33 +4,34 @@ import requests
 import os
 from models import db, User, Points, Event
 from dotenv import load_dotenv
-from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
+from flask_jwt_extended import (
+    create_access_token,
+    get_jwt_identity,
+    jwt_required,
+    JWTManager,
+)
 from flask_cors import CORS
 
 load_dotenv()
 
-DISCORD_CLIENT_ID = os.getenv('DISCORD_CLIENT_ID')
-DISCORD_CLIENT_SECRET = os.getenv('DISCORD_CLIENT_SECRET')
-REDIRECT_URI = 'http://127.0.0.1:5000/discord/callback'
-FELLOWSHIP_GUILD_ID = '818888976458973224'
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-CURRENT_FELLOWSHIP = '0'
+DISCORD_CLIENT_ID = os.getenv("DISCORD_CLIENT_ID")
+DISCORD_CLIENT_SECRET = os.getenv("DISCORD_CLIENT_SECRET")
+REDIRECT_URI = "http://127.0.0.1:5000/discord/callback"
+FELLOWSHIP_GUILD_ID = "818888976458973224"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CURRENT_FELLOWSHIP = "0"
 
-DB_USER = os.getenv('DB_USER')
-DB_PW = os.getenv('DB_PW')
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
-DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv("DB_USER")
+DB_PW = os.getenv("DB_PW")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
-FRONTEND_URL = os.environ['FRONTEND_URL']
-SECRET_KEY = os.environ['SECRET_KEY']
+FRONTEND_URL = os.environ["FRONTEND_URL"]
+SECRET_KEY = os.environ["SECRET_KEY"]
 
-db_uri = 'postgresql://{dbuser}:{dbpw}@{dbhost}:{dbport}/{dbname}'.format(
-    dbuser=DB_USER,
-    dbpw=DB_PW,
-    dbhost=DB_HOST,
-    dbport=DB_PORT,
-    dbname=DB_NAME
+db_uri = "postgresql://{dbuser}:{dbpw}@{dbhost}:{dbport}/{dbname}".format(
+    dbuser=DB_USER, dbpw=DB_PW, dbhost=DB_HOST, dbport=DB_PORT, dbname=DB_NAME
 )
 
 app = Flask(__name__)
@@ -39,7 +40,7 @@ app.config.update(
     SQLALCHEMY_DATABASE_URI=db_uri,
     SQLALCHEMY_TRACK_MODIFICATIONS=False,
     SECRET_KEY=SECRET_KEY,
-    JWT_SECRET_KEY=SECRET_KEY
+    JWT_SECRET_KEY=SECRET_KEY,
 )
 
 db.init_app(app)
@@ -52,12 +53,12 @@ def index():
     return f"Hello {session.get('username')}#{session.get('discriminator')} @ {session.get('role')}"
 
 
-@app.route('/discord')
+@app.route("/discord")
 def discord():
-    full_redirect_url = 'https://discord.com/api/oauth2/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code&scope={scope}'.format(
+    full_redirect_url = "https://discord.com/api/oauth2/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code&scope={scope}".format(
         client_id=DISCORD_CLIENT_ID,
         redirect_uri=REDIRECT_URI,
-        scope='identify email guilds'
+        scope="identify email guilds",
     )
     return redirect(full_redirect_url)
 
@@ -69,24 +70,27 @@ def discord_callback():
     Discord Access Token is stored in session.get('discord_access_token')
     """
     # Get the discord access token
-    data = requests.post("https://discord.com/api/oauth2/token", data={
-        'client_id': DISCORD_CLIENT_ID,
-        'client_secret': DISCORD_CLIENT_SECRET,
-        'grant_type': 'authorization_code',
-        'code': request.args.get("code"),
-        'redirect_uri': REDIRECT_URI,
-        'scope': 'identify email guilds'
-    }, headers={
-        'Content-Type': 'application/x-www-form-urlencoded'
-    })
+    data = requests.post(
+        "https://discord.com/api/oauth2/token",
+        data={
+            "client_id": DISCORD_CLIENT_ID,
+            "client_secret": DISCORD_CLIENT_SECRET,
+            "grant_type": "authorization_code",
+            "code": request.args.get("code"),
+            "redirect_uri": REDIRECT_URI,
+            "scope": "identify email guilds",
+        },
+        headers={"Content-Type": "application/x-www-form-urlencoded"},
+    )
 
     # Store access token in session
     session["discord_access_token"] = data.json()["access_token"]
 
     # Get user's information
-    data = requests.get("https://discord.com/api/v8/users/@me", headers={
-        "Authorization": f"Bearer {session.get('discord_access_token')}"
-    })
+    data = requests.get(
+        "https://discord.com/api/v8/users/@me",
+        headers={"Authorization": f"Bearer {session.get('discord_access_token')}"},
+    )
 
     email = data.json()["email"]
     discriminator = data.json()["discriminator"]
@@ -99,9 +103,10 @@ def discord_callback():
     session["discriminator"] = discriminator
 
     # get all the guilds that user's in
-    guilds = requests.get("https://discord.com/api/v8/users/@me/guilds", headers={
-        "Authorization": f"Bearer {session.get('discord_access_token')}"
-    })
+    guilds = requests.get(
+        "https://discord.com/api/v8/users/@me/guilds",
+        headers={"Authorization": f"Bearer {session.get('discord_access_token')}"},
+    )
 
     # check if the user is in the fellowship guide
     in_fellowship = False
@@ -113,16 +118,18 @@ def discord_callback():
         message = "Error: User is not a current MLH fellow!"
         return redirect(f"{FRONTEND_URL}?error=true&msg={message}")
     else:
-        role = requests.get(f"https://discord.com/api/v8/guilds/{FELLOWSHIP_GUILD_ID}/members/{session.get('discord_id')}", headers={
-            "Authorization": f"Bot {BOT_TOKEN}"
-        })
+        role = requests.get(
+            f"https://discord.com/api/v8/guilds/{FELLOWSHIP_GUILD_ID}/members/{session.get('discord_id')}",
+            headers={"Authorization": f"Bot {BOT_TOKEN}"},
+        )
 
         # assume user only have one role
         user_roles = role.json()["roles"]
 
-        roles = requests.get(f"https://discord.com/api/v8/guilds/{FELLOWSHIP_GUILD_ID}/roles", headers={
-            "Authorization": f"Bot {BOT_TOKEN}"
-        })
+        roles = requests.get(
+            f"https://discord.com/api/v8/guilds/{FELLOWSHIP_GUILD_ID}/roles",
+            headers={"Authorization": f"Bot {BOT_TOKEN}"},
+        )
 
         role = None
         for r in roles.json():
@@ -137,8 +144,7 @@ def discord_callback():
         if User.query.filter_by(id=discord_id).first():
             message = "Success: Logged in!"
         else:
-            new_user = User(id=discord_id, name=username,
-                            email=email, role=role)
+            new_user = User(id=discord_id, name=username, email=email, role=role)
             db.session.add(new_user)
             db.session.commit()
             message = "Success: User registered!"
@@ -147,7 +153,7 @@ def discord_callback():
     return redirect(f"{FRONTEND_URL}?token={jwt_token}&msg={message}")
 
 
-@app.route("/admin/create_event", methods = ["POST"])
+@app.route("/admin/create_event", methods=["POST"])
 def create_event():
     """Create an event.
 
@@ -156,19 +162,22 @@ def create_event():
     """
     data = request.form
 
-    event_name = data['name']
-    start_time_f = data['start_time']
-    end_time_f = data['end_time']
-    link = data['event_link']
-    secret_code_f = data['secret_code']
-    points = data['points_amount']
+    event_name = data["name"]
+    start_time_f = data["start_time"]
+    end_time_f = data["end_time"]
+    link = data["event_link"]
+    secret_code_f = data["secret_code"]
+    points = data["points_amount"]
     event_id = None
 
-    new_event = Event(name=event_name, start_time=start_time_f,
-                        end_time=end_time_f,
-                        points_amount=points,
-                        secret_code=secret_code_f,
-                        event_link=link)
+    new_event = Event(
+        name=event_name,
+        start_time=start_time_f,
+        end_time=end_time_f,
+        points_amount=points,
+        secret_code=secret_code_f,
+        event_link=link,
+    )
 
     try:
         db.session.add(new_event)
@@ -179,21 +188,16 @@ def create_event():
 
         event_id = new_event.id
 
-        return jsonify({
-            "success": success,
-            "message": message,
-            "id": event_id
-        })
+        return jsonify({"success": success, "message": message, "id": event_id})
+
     except:
         message = "Server Error. Could not commit to database"
         success = False
 
-        return jsonify({
-            "success": success,
-            "message": message
-        })
+        return jsonify({"success": success, "message": message})
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run()
